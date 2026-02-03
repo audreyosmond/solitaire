da361d62f808655416da67077666a055d2c3faf7
audreyosmond <audreyosmondo2@gmail.com>
2026-02-03 09:25:11 -0600

    inputs and stuff yknow

--- main.java
+++ main.java
@@ -2,6 +2,39 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        
+        //game setup
+        gameState game = new gameState();
+        deck cards=new deck();
+        game.newGame(cards.newGame());
+        boolean playing=true;
+
+        //input
+        Scanner input=new Scanner(System.in);
+        while(playing){
+            System.out.print("Move card, Draw card, or Score card? ");
+            String choice=input.next();
+            System.out.println();
+            choice=choice.toLowerCase();
+
+            if(choice.equals("move")){
+                System.out.print("Which column are you moving from? ");
+                int first = input.nextInt();
+                System.out.println();
+                System.out.print("Which card are you moving? ");
+                int cardNumber = input.nextInt();
+                System.out.println();
+                System.out.print("Which column are you moving to? ");
+                int second = input.nextInt();
+                System.out.println();
+
+                game.validateMoveCard(first-1, second-1, cardNumber-1);
+            }
+            if(choice.equals("draw")){
+                game.draw();
+            }
+            if(choice.equals("score")){
+
+            }
+        }
     }
 }
